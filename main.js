6ba29be94f5abd4eb6915edacf5668d91815e950
liveforone <yc4852@gmail.com>
2022-04-16 14:08:07 +0900

    Good Commit

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let http = require('http');
 let fs = require('fs');
 let url = require("url");
+let qs = require("querystring");
 
 const templateHTML = (title, list, body) => {
   return `<!doctype html>
@@ -12,6 +13,7 @@ const templateHTML = (title, list, body) => {
     <body>
       <h1><a href="/">WEB</a></h1>
       ${list}
+      <a href="/create">create</a>
       ${body}
     </body>
   </html>`;
@@ -59,6 +61,37 @@ let app = http.createServer((request,response) => {
           });
         });
       }
+    } else if (pathname === "/create") {
+      fs.readdir('./data', (error, fileList) => {
+        fs.readFile(`data/${queryData.get("id")}`, 'utf8', (err, description) => {
+          let title = "WEB-create";
+          let list = templateList(fileList);
+          let template = templateHTML(title, list, `
+            <form action="http://localhost:3000/process_create" method="post">
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p>
+              <textarea name="description" placeholder="description"></textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+            </form>
+          `);
+          response.writeHead(200);
+          response.end(template);
+        });
+      });
+    } else if (pathname ==="/process_create") {
+      let body = "";
+      request.on("data", (data) => {
+        body += data;
+      });
+      request.on("end", () => {
+        let title = new URLSearchParams(body).get("title");
+        let description = new URLSearchParams(body).get("description");
+      });
+      response.writeHead(200);
+      response.end("sucess");
     } else {
 
       response.writeHead(404);
